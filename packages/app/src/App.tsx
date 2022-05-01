838ce27b16442929c24e02082130095efa21949e
DevOps_user_hk-at-794756280088 <ariaslsk26@nate.com>
2022-05-01 17:34:55 +0900

    first commit

---
+++ packages/app/src/App.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import { Button } from '@leehankue/components';
+import './App.css';
+
+function App() {
+  const [state, setState] = React.useState(0);
+  return (
+    <div className="App">
+      {state}
+     <Button onClick={()=>setState(s=>++s)}>Click me2!</Button>
+    </div>
+  );
+}
+
+export default App;
